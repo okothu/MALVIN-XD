155e046c1205fbad200553c5a4d2b09a9ccb5c13
okothu <164142314+okothu@users.noreply.github.com>
2025-07-26 09:55:37 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~YvUyGJCJ#xEWSXkHK9STZfkYsyheYM_ygJMO6Wb75FgD23yvghm8 ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "254785450636", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
